5315593cf866cf558ac768fd25f35c0591b355cd
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-09-04 17:05:07 +0900

    hotfix: CONFLICT_RESOURCE(중복된 자원에 대한 요청) 오류코드 추가

--- src/main/java/com/gapple/backend/common/exception/ErrorCode.java
+++ src/main/java/com/gapple/backend/common/exception/ErrorCode.java
@@ -7,6 +7,7 @@ public enum ErrorCode implements CommonErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰 만료"),
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "파라미터 값 확인 필요"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 리소스 없음"),
+    CONFLICT_RESOURCE(HttpStatus.CONFLICT, "중복된 리소스"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류")
     ;
 
